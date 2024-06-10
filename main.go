3f07d89a4e375dd9f516f8cc74f176117e42340f
snowlight-aemt <it.s.aemt@gmail.com>
2024-06-10 21:14:18 +0900

    refactoring 정보 수정

--- main.go
+++ main.go
@@ -15,13 +15,10 @@ func main() {
 	c := make(chan httpResult)
 	urls := []string{
 		"https://www.youtube.com/",
-		"https://fastcampus.co.kr/",
-		"https://www.inflearn.com/",
 		"https://www.google.com/",
-		"https://www.reddit.com/",
-		//"https://www.airbnb.co.kr/",
+		"https://www.airbnb.co.kr/",
 		"https://aws.amazon.com/",
-		"https://yomou.syosetu.com/",
+		"https://www.facebook.com/",
 	}
 
 	for _, url := range urls {
@@ -39,10 +36,21 @@ func main() {
 }
 
 func hitURL(url string, result chan<- httpResult) {
-	resp, err := http.Get(url)
+	req, err := http.NewRequest("GET", url, nil)
+	//req.Header.Add("Accept-Encoding", "gzip, deflate")
+	//req.Header.Add("Accept", "*/*"
+	//req.Header.Add("Accept-Connection", "keep-alive")
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	//resp, err := http.Get(url)
 
 	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(resp.StatusCode)
+		fmt.Println(resp.Body)
+
 		status = "FAILED"
 	}
 
